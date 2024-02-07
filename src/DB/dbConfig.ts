c03bbe46eefd95a13cebce9b69f0cc016a262ce5
Nayan Desai <ndmaratha01@gmail.com>
2024-02-07 11:30:47 +0530

    first commit

---
+++ src/DB/dbConfig.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+export const connect = async () => {
+	try {
+        mongoose.connect(process.env.MONGO_URI!)
+        const connection=mongoose.connection;
+        connection.on('connected',()=>{
+            console.log("successfully connected to mongodb")
+        })
+        connection.on('error',(err)=>{
+            console.log("cannot connect to mongodb"+err)
+        })
+	} catch (error) {
+		console.log("something went wrong when connection to db mongo");
+		console.log(error);
+	}
+};
